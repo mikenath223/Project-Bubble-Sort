53e6657e672acc07fafb20890fb381d519c64167
mike <michgoldennathan17@gmail.com>
2019-11-14 21:40:03 +0100

    Update: added bubble sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
-def bubble_sort(arr); end
+def bubble_sort(arr)
+  if arr.length < 1
+    return arr 
+  end
+    i = 0
+    while true
+      if i + 1 == arr.length
+        break
+      end
+      if arr[i] > arr[i + 1]
+        # puts arr[i]
+      yield(arr[i],arr[i + 1])
+      end
+      i += 1
+    end
+  puts arr
+end
+
+bubble_sort([4,3,78,2,0,2]) {|first,second|
+  new_second = second
+  second = first
+  first = new_second
+}
